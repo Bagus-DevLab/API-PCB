34d33b84ca2543b24b012b02cc1656bf6d66477c
BagusA23 <bagusanardiansyah@gmail.com>
2026-01-26 23:01:25 +0700

    Add Rate Limiter using Redis

--- app/api/v1/logs.py
+++ app/api/v1/logs.py
@@ -2,24 +2,24 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 
-from app.db.database import get_db  # Pastikan path ini sesuai dengan file koneksi DB kamu
+# --- IMPORT RATE LIMITER ---
+from fastapi_limiter.depends import RateLimiter
+
+from app.db.database import get_db
 from app.crud import log as crud_log
 from app.schemas.log import LogResponse
 
 router = APIRouter()
 
-@router.get("/{device_id}", response_model=List[LogResponse])
+# --- TEMPELKAN DI SINI (dependencies) ---
+# times=5, seconds=10 artinya: Max 5 request dalam 10 detik.
+@router.get("/{device_id}", response_model=List[LogResponse], dependencies=[Depends(RateLimiter(times=5, seconds=10))])
 def read_device_logs(
     device_id: str, 
     limit: int = 20, 
     db: Session = Depends(get_db)
 ):
-    """
-    Ambil histori data sensor.
-    Contoh: GET /api/logs/ALAT_PALSU_01?limit=5
-    """
     logs = crud_log.get_logs_by_device(db, device_id=device_id, limit=limit)
     if not logs:
-        # Kita tidak return 404, tapi list kosong saja biar frontend gak error
         return []
     return logs
