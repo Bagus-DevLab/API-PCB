2ef2b8f1650794a9acaee6d23914255fb9e4cc77
BagusA23 <bagusanardiansyah@gmail.com>
2026-01-27 12:03:11 +0700

    Refactor Log schemas to include optional relay fields and improve data handling

--- app/schemas/log.py
+++ app/schemas/log.py
@@ -1,25 +1,30 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional # <--- WAJIB IMPORT INI
 
-# Cetakan dasar (isi data)
 class LogBase(BaseModel):
-    topic_type: str
-    value: str
-
-class LogResponse(BaseModel):
-    id: int
     device_id: str
     temperature: float
     humidity: float
     amonia: float
     feed_level: float
-    
-    # Tambahkan status relay ini
-    relay_1: bool # Lampu
-    relay_2: bool # Kipas/Pompa Minum
-    relay_3: bool # Pompa Siram
-    
+    # relay boleh tidak diisi saat create
+    relay_1: Optional[bool] = None 
+    relay_2: Optional[bool] = None
+    relay_3: Optional[bool] = None
+
+class LogCreate(LogBase):
+    pass
+
+class LogResponse(LogBase):
+    id: int
     created_at: datetime
+    
+    # KITA TIMPA LAGI DISINI BIAR AMAN
+    # Kalau data dari DB null/gak ada, dia bakal isi None (gak error)
+    relay_1: Optional[bool] = None
+    relay_2: Optional[bool] = None
+    relay_3: Optional[bool] = None
 
     class Config:
         from_attributes = True
